47e924c7c9960aa02484ebe73bbfbc6285cf6ec0
nscgotomydream <18810487612@163.com>
2016-08-16 18:42:05 +0800

    1.2
    token

--- Addons/V/Controller/Token/Token.php
+++ Addons/V/Controller/Token/Token.php
@@ -16,49 +16,16 @@ class Token
     public function doAccesstokenPost()
     {
         $token = model('Token')->accessToken(req('Post'));
-<<<<<<< HEAD
         if (empty($token)) {
             $this->AjaxReturn([
                 'code' => -200,
                 'msg' => '获取失败',
                 'data' => "" ]);
         } else {
-=======
-<<<<<<< HEAD
-        if(empty($token)){
-            $this->AjaxReturn([
-                'code' => -200,
-                'msg' => '获取失败',
-                'data' =>""
-            ]);
-        }else{
-=======
-        if (empty($token)) {
-            $this->AjaxReturn([
-                'code' => -200,
-                'msg' => '获取失败',
-                'data' => ""
-            ]);
-        } else {
->>>>>>> refs/remotes/origin/master
->>>>>>> origin/masterAddons
             $this->AjaxReturn([
                 'data' => $token
             ]);
         }
     }
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-    public function doIndex()
-    {
-        view('',[
-            'verify'=>MD5('dsaffsd1cda067b175ab0e9e1fdfe8dcd7d71ff1471276800')
-        ]);
-    }
-
-=======
->>>>>>> refs/remotes/origin/master
->>>>>>> origin/masterAddons
 }
