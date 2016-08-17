4515968b4d70107bb0b076341043e72683d27586
nscgotomydream <18810487612@163.com>
2016-08-17 12:30:34 +0800

    1.2
    token

--- Addons/V/Model/Token.php
+++ Addons/V/Model/Token.php
@@ -16,10 +16,6 @@ class Token implements ModelInterface
     private $config=array();
     private $clientSecret='';
     private $expires='';
-    private $deviceId='';
-    private $login = '';
-    private $token ='';
-    private $userId = '';
     public function __construct()
     {
         $this->config = server()->Config('Config')['token'];
@@ -32,37 +28,51 @@ class Token implements ModelInterface
     {
         return [
             'Server::Db',
-            'Model::User'
+            'Model::User',
+            'Model::Gate'
         ];
     }
     //获取token表信息
-    public function getToken($accessToken)
+    public function getTokenInfo($accessToken)
     {
-        $check = $this->getTokenInfo($accessToken);$info = $check?$check:[];
+        $check = $this->getTokenInfoFromSql($accessToken);$info = $check?$check:[];
         return $info;
     }
     //获取通行证accessToken
     public function accessToken($req)
     {
-        $this->checkReq($req);
-        $user = model('User')->getUserByLogin($this->login);
-        $userId = $user['userId'];
-        $this->userId = $userId;
-        if(!$userId)return [];
         //删除原token
-        $this->deleteTokenByUserId($userId);
+        $this->deleteTokenByUserId(bus('userId'));
         //生成token
         $this->token();
         //添加token
         $checkInsertToken = $this->addToken();
         if($checkInsertToken){
             return [
-            'token'     =>$this->token,
+            'token'     =>bus('token'),
             'expires'   =>$this->expires,
             ];
         }else{
             return [];
         }
+    }
+
+    public function validateTokenReq($req)
+    {
+        //校验参数是否为空
+        $validate = ['verify','login','time','deviceId'];
+        if(!model('Gate')->isExistParams($validate,$req))return false;
+        if(!model('Gate')->isEmpty($req))return false;
+        //校验secrect密匙verify
+        $this->verify($req);
+        //校验login对应的用户是否存在
+        $user = model('User')->getUserByLogin($req['login']);
+        if(empty($user))return false;
+        bus([
+            'userId'=> $user['userId'],
+            'login'=> $req['login']
+        ]);
+        return true;
     }
 
     //数据库操作
@@ -74,7 +84,7 @@ class Token implements ModelInterface
         $check = $delete?true:false;
         return $check;
     }
-    private function getTokenInfo($accessToken)
+    private function getTokenInfoFromSql($accessToken)
     {
         $tokenInfo = server('Db')->getRow("select * from token where `accessToken`='$accessToken'");
         $tokenInfo = $tokenInfo?$tokenInfo:[];
@@ -83,9 +93,9 @@ class Token implements ModelInterface
 
     private function addToken()
     {
-        $res['login']       = $this->login;
-        $res['userId']      = $this->userId;
-        $res['accessToken'] = $this->token;
+        $res['login']       = bus('login');
+        $res['userId']      = bus('userId');
+        $res['accessToken'] = bus('token');
         $res['createAt']    = time();
         $res['expires_in']  = 2592000;
         $insert = server('Db')->autoExecute('token', $res, 'INSERT');
@@ -93,30 +103,25 @@ class Token implements ModelInterface
         return $check;
     }
 
-    private function checkReq($req)
-    {
-        if(!$req)return [];
-        $deviceId   = $req['deviceId'];
-        $login      = $req['login'];
-        $time       = $req['time'];
-        $verify     = $req['verify'];
-        if(!$deviceId&&!$login&&!$time&&!$verify)return [];
-        if(!(is_string($deviceId)&&is_string($login)&&is_string($time)&&is_string($verify)))return[];
-        //TODO:调用设备校验
-        if ($verify != MD5($deviceId . $this->clientSecret . $login . $time))return [];
-        $this->deviceId = $deviceId;
-        $this->login = $login;
-    }
-
     //TODO:算法待修改，一个用户可能有多台设备，设备编号不唯一，加入deviceId，同一用户token不唯一
     private function token()
     {
-        $this->token = md5($this->login . '_' . microtime(true) . '_' . rand(100000000, 999999999));
+        $token = md5(bus('login') . '_' . microtime(true) . '_' . rand(100000000, 999999999));
+        bus([
+           'token'=>$token
+        ]);
     }
 
-    private function validateDevice()
+    private function verify($req)
     {
+        $verify = $req['verify'];
+        $deviceId = $req['deviceId'];
+        $login = $req['login'];
+        $time = $req['time'];
         //TODO: 校验设备编号正确性
+        if ($verify != MD5($deviceId . $this->clientSecret . $login . $time))return [];
     }
+
+
 
 }
