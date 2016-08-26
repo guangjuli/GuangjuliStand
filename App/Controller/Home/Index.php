9e947397a1f40b96423b3109a30808e9b19bbe45
shampeak <976107540@qq.com>
2016-08-26 17:05:23 +0800

    8.26

---
+++ App/Controller/Home/Index.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+
+class Home extends BaseController
+{
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    /**
+     * 后台首页
+     */
+    public function doIndex()
+    {
+        //view();
+    }
+
+
+}
