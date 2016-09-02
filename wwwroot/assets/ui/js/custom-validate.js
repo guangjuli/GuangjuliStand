e46538caf46d12dbe3d5a2ae9922e41806a13260
nscgotomydream <18810487612@163.com>
2016-09-02 17:22:48 +0800

    1.3
    user表管理，待修改

---
+++ wwwroot/assets/ui/js/custom-validate.js
@@ -0,0 +1,88 @@
+function customValidate(id){
+    $(document).ready(function() {
+        $("#"+id).validate({
+            onfocusout: function(element) { $(element).valid(); },
+            errorPlacement: function(error, element) {
+                // Append error within linked label
+                $(element).parent().next().children().remove();
+                $(element).parent().next().append( error);
+            },
+            rules: {
+                nickName: {
+                    required: true,
+                    minlength: 2
+                },
+                login : {
+                    required : true,
+                    minlength : 11,
+                    // 自定义方法：校验手机号在数据库中是否存在
+                    // checkPhoneExist : true,
+                    isMobile : true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                    minlength: 6,
+                    isPassword:true
+                },
+                confirm_password: {
+                    required: true,
+                    minlength: 6,
+                    equalTo: "#password",
+                    isPassword:true
+                }
+            },
+            messages: {
+                nickName: "请输入昵称",
+                email: {
+                    required: "请输入Email地址",
+                    email: "请输入正确的email地址"
+                },
+                login : {
+                    required : "请输入手机号",
+                    minlength: "请正确输入",
+                    isMobile : "请输入手机号码"
+                },
+                password: {
+                    required: "请输入密码",
+                    minlength: "不小于6个字符",
+                    isPassword:"密码格式错误"
+                },
+                confirm_password: {
+                    required: "请输入确认密码",
+                    minlength: "不小于6个字符",
+                    equalTo: "两次输入不一致",
+                    isPassword:"密码格式错误"
+                }
+            }
+        });
+    });
+}
+
+
+jQuery.validator.addMethod("isMobile", function(value, element) {
+    var length = value.length;
+    var mobile = /^(13[0-9]{9})|(18[0-9]{9})|(14[0-9]{9})|(17[0-9]{9})|(15[0-9]{9})$/;
+    return this.optional(element) || (length == 11 && mobile.test(value));
+}, "请正确填写您的手机号码");
+
+jQuery.validator.addMethod("isPassword", function(value, element) {
+    var password = /^[a-zA-Z]+[a-zA-Z0-9_]{6,19}$/;
+    return this.optional(element) || (password.test(value));
+}, "密码格式错误");
+
+function showErrorMsg(error){
+    for( var o in error){
+        $("input").each(
+            function(){
+                if($(this).attr("name")== o){
+                    $(this).parent().next().children().remove();
+                    $(this).parent().next().append("<div>"+error[o]+"</div>");
+                }
+            }
+        );
+    }
+}
